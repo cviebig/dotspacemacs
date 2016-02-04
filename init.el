f8af1c6b7edfa35cdb0b6d946e1338e0da0c14b3
Christoph Viebig <git@christoph-viebig.de>
2016-02-04 17:33:55 +0100

    Remove commented out layers

--- init.el
+++ init.el
@@ -23,17 +23,9 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     ;; auto-completion
-     ;; better-defaults
      emacs-lisp
      git
-     ;; markdown
      org
-     ;; (shell :variables
-     ;;        shell-default-height 30
-     ;;        shell-default-position 'bottom)
-     ;; spell-checking
-     ;; syntax-checking
      version-control
      )
    ;; List of additional packages that will be installed without being
