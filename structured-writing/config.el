ee4093b3b4024041b47d316a77712e51e08bbbd0
Christoph Viebig <git@christoph-viebig.de>
2016-02-10 20:48:18 +0100

    Refactor for configurable publishing properties

--- structured-writing/config.el
+++ structured-writing/config.el
@@ -18,41 +18,76 @@
                     "Ideation"
                     "Strategy"
                     "Literature"
+                    "Reference"
                     "Examples"
                     "Scratch"
                     "Notes"
-                    "Text"
-                    "References"))
+                    "Text"))
 
 (setq org-tags-exclude-from-inheritance my-org-tags)
 
+;;; properties that will be merged into the
+;;; on-the-fly generated org-publish-project-alist.
+(setq my-org-publish-properties-text '(
+    :latex-class "scrartcl"
+    :latex-class-options "[twocolumn]"
+    :headline-levels 5
+    :with-drawers ("EXPOSEABSTRACT")
+    :with-toc nil
+))
+
+(setq my-org-publish-properties-others '(
+    :latex-class "scrartcl"
+    :headline-levels 5
+    :with-toc nil
+))
+
 (make-my-org-prepare-export-functions "structure"
+                                      "3"
                                       '(my-org-prepare-unique-tag "Structure")
-                                       "3")
+                                      my-org-publish-properties-others)
+
 (make-my-org-prepare-export-functions "survey"
+                                      "w"
                                       (my-org-prepare-multi-tag "Survey")
-                                      "w")
+                                      my-org-publish-properties-others)
+
 (make-my-org-prepare-export-functions "goals"
+                                      "g"
                                       (my-org-prepare-unique-tag "Goals")
-                                      "g")
+                                      my-org-publish-properties-text)
+
 (make-my-org-prepare-export-functions "ideation"
+                                      "s"
                                       (my-org-prepare-multi-tag "Ideation")
-                                      "s")
+                                      my-org-publish-properties-others)
+
 (make-my-org-prepare-export-functions "strategy"
+                                      "t"
                                       (my-org-prepare-unique-tag "Strategy")
-                                      "t")
+                                      my-org-publish-properties-others)
+
 (make-my-org-prepare-export-functions "literature"
+                                      "e"
                                       (my-org-prepare-unique-parental-tag "Literature" "Reference")
-                                      "e")
+                                      my-org-publish-properties-others)
+
 (make-my-org-prepare-export-functions "examples"
+                                      "q"
                                       (my-org-prepare-unique-tag "Examples")
-                                      "q")
+                                      my-org-publish-properties-others)
+
 (make-my-org-prepare-export-functions "scratch"
+                                      "a"
                                       (my-org-prepare-unique-tag "Scratch")
-                                      "a")
+                                      my-org-publish-properties-others)
+
 (make-my-org-prepare-export-functions "notes"
+                                      "d"
                                       (my-org-prepare-unique-tag "Notes")
-                                      "d")
+                                      my-org-publish-properties-others)
+
 (make-my-org-prepare-export-functions "text"
+                                      "f"
                                       (my-org-prepare-unique-tag "Text")
-                                      "f")
+                                      my-org-publish-properties-text)
