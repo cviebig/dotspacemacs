9f0eb417261e5d10a1ad5f2c999089b5774a5844
Christoph Viebig <git@christoph-viebig.de>
2016-11-03 11:27:07 +0100

    Add `spacemacs-dark` theme

--- init.el
+++ init.el
@@ -99,7 +99,8 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(deeper-blue
-                         zenburn)
+                         zenburn
+			 spacemacs-dark)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
